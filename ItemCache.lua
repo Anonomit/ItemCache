d152066aa52219b8d1b7dd885ece78d573ec5225
Anonomit <anonomit@gmail.com>
2024-09-25 16:42:26 -0400

    Release 1.9.0

--- ItemCache.lua
+++ ItemCache.lua
@@ -4,7 +4,7 @@ local ADDON_NAME = "ItemCache"
 local HOST_ADDON_NAME, Data = ...
 local IsStandalone = ADDON_NAME == HOST_ADDON_NAME
 
-local MAJOR, MINOR = ADDON_NAME, 12
+local MAJOR, MINOR = ADDON_NAME, 13
 local ItemCache, oldMinor = LibStub:NewLibrary(MAJOR, MINOR)
 if not ItemCache and not IsStandalone then
   return
